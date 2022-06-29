7d4394eac9c8d75b612cda5d32451e0fed1769e8
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-29 13:30:46 +0200

    replaced json2xml with fast-xml-parsr

--- qxml.js
+++ qxml.js
@@ -1,6 +1,7 @@
-import fs from 'fs';
-import * as qsys from './qsys.js';
-import parser from 'xml2json';
+import fs from "fs";
+import * as qsys from "./qsys.js";
+// import parser from 'xml2json';
+import { XMLParser } from "fast-xml-parser";
 
 /**
  * Convert XML file to JavaScript object.
@@ -16,10 +17,12 @@ import parser from 'xml2json';
  * }
  */
 export const getXmlFileAsObject = (pathAndFileName) => {
-	const fullPathAndFileName =
-		qsys.buildSystemAbsolutePathAndFileName(pathAndFileName);
-	const xmlAsText = fs.readFileSync(fullPathAndFileName, 'utf8');
-	const xmlAsJson = parser.toJson(xmlAsText);
-	const xmlAsObject = JSON.parse(xmlAsJson);
-	return xmlAsObject;
+  const fullPathAndFileName =
+    qsys.buildSystemAbsolutePathAndFileName(pathAndFileName);
+  const xmlAsText = fs.readFileSync(fullPathAndFileName, "utf8");
+  // const xmlAsJson = parser.toJson(xmlAsText);
+  const parser = new XMLParser();
+  // const xmlAsObject = JSON.parse(xmlAsJson);
+  const xmlAsObject = parser.parse(xmlAsText);
+  return xmlAsObject;
 };
